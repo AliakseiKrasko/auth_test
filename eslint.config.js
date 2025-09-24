d8295c3f89f1a82ff96275b7da39351a5347ba83
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-09-24 21:56:17 +0300

    feat: established dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { globalIgnores } from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
-export default defineConfig([
+export default tseslint.config([
+  ...pluginQuery.configs['flat/recommended'],
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
